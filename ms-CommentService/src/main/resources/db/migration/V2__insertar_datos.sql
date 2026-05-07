a67488ffba59e17b238ed662e9a24eee0cdd19da
Albedo-AB <di.aravenab@duocuc.cl>
2026-05-07 16:13:00 -0400

    insertardatos decomement67

---
+++ ms-CommentService/src/main/resources/db/migration/V2__insertar_datos.sql
@@ -0,0 +1,14 @@
+INSERT INTO comments (CONTENIDO, ID_USUARIO, ID_POST, FECHA_CREACION)
+VALUES ('¡Excelente artículo! Me sirvió mucho para mi proyecto.', 101, 50, NOW());
+
+INSERT INTO comments (CONTENIDO, ID_USUARIO, ID_POST, FECHA_CREACION)
+VALUES ('No estoy de acuerdo con el segundo punto, creo que falta profundidad.', 102, 50, NOW());
+
+INSERT INTO comments (CONTENIDO, ID_USUARIO, ID_POST, FECHA_CREACION)
+VALUES ('¿Alguien sabe si esto aplica también para microservicios con Go?', 105, 52, NOW());
+
+INSERT INTO comments (CONTENIDO, ID_USUARIO, ID_POST, FECHA_CREACION)
+VALUES ('Muchas gracias por compartir esta información, muy clara.', 110, 55, NOW());
+
+INSERT INTO comments (CONTENIDO, ID_USUARIO, ID_POST, FECHA_CREACION)
+VALUES ('He intentado replicar el ejemplo pero obtengo un error en el paso 3.', 101, 52, NOW());
