197bc36b0c41c819b9abd8e66b65e364a8e9b6fd
Ricks86 <ju.fernandeza@duocuc.cl>
2026-06-11 17:18:32 -0400

    API GATEWAY implementado

--- Gateway/src/main/java/com/API/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/API/Gateway/GatewayApplication.java
@@ -2,8 +2,15 @@ package com.API.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		ReactiveSecurityAutoConfiguration.class
+})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
